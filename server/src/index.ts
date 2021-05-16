59bf01c80fe14a3b209a7365484d67db80dcec76
githiago-f <tfarias@protonmail.com>
2021-05-16 20:57:40 -0300

    suport for cors added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 process.env.UV_THREAD_POOL = '1';
 import express, { Router } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { CityController } from './controller/city-controller';
 import { CompanyController } from './controller/company-controller';
 import { CustomerController } from './controller/customer-controller';
@@ -12,7 +13,7 @@ const conn = isProduction() ? prodConnection() : devConnection();
 const app = express();
 
 app.use(morgan(isProduction() ? 'short' : 'dev'));
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/city', CityController(Router(), conn));
