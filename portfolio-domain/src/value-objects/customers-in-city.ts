858f5eae15171ffe05944fdf0f2e85ec63446bbf
githiago-f <tfarias@protonmail.com>
2021-05-16 20:56:07 -0300

    added city_id to response schema

--- portfolio-domain/src/value-objects/customers-in-city.ts
+++ portfolio-domain/src/value-objects/customers-in-city.ts
@@ -1,11 +1,13 @@
 import { City } from '../entity';
 
 export type CustomersInCity = {
+  city_id: number;
   city: string;
   customers_total: number;
 }
 
 export const CustomersInCity = (city: City, customers: number) => ({
+  city_id: city.id,
   city: city.name,
   customers_total: customers
 });
