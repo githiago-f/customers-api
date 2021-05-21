7731a095567b6a1a953f3ec614c44f7ceea71bac
githiago-f <tfarias@protonmail.com>
2021-05-21 00:38:49 -0300

    testing heroku database_url

--- server/src/util/connections.ts
+++ server/src/util/connections.ts
@@ -2,13 +2,16 @@ import { knex, Knex } from 'knex';
 
 export type DBConnection = Knex<any, unknown[]>;
 
-export const prodConnection = () => knex({
-  client: 'mysql2',
-  connection: process.env.DATABASE_URL || {
-    host: process.env.DB_HOST||'db',
-    database: process.env.DB_NAME||'application',
-    password: process.env.DB_PASSWORD||'',
-    user: process.env.DB_USERNAME||'root',
-    port: parseInt(process.env.DB_PORT|| '3306')
-  }
-});
+export const prodConnection = () => {
+  console.log(process.env.DATABASE_URL);
+  return knex({
+    client: 'mysql2',
+    connection: process.env.DATABASE_URL || {
+      host: process.env.DB_HOST||'db',
+      database: process.env.DB_NAME||'application',
+      password: process.env.DB_PASSWORD||'',
+      user: process.env.DB_USERNAME||'root',
+      port: parseInt(process.env.DB_PORT|| '3306')
+    }
+  });
+};
